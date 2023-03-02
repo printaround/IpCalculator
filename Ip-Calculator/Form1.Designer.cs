9cd0f0b2350432b1e2f31958a1ef90a9cb95e22e
printaround <68951382+printaround@users.noreply.github.com>
2023-03-02 14:27:10 +0300

    Print Around
    Print Around

---
+++ Ip-Calculator/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ip_calc_by_DAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 129F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 135F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 113F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.969231F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(794, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ввод данных:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.969231F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(794, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9.969231F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(794, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Романычев Даниил, ЭИСБ-24";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 16.06154F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(3, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(794, 123);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.PowderBlue;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 8.861538F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(3, 182);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(794, 129);
+            this.textBox2.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 317);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 107);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Consolas", 16.06154F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(391, 101);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Consolas", 16.06154F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(400, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(391, 101);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выйти";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IP-calc by DAR";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ip-Calculator/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ip_calc_by_DAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            string data = "10.10.10.10/25";
+
+            data = textBox1.Text;   //входные данные
+            if(data.Length > 18)    //проверка на количество исходных данных
+            {
+                textBox2.Text += "Слишком много данных, что то пошло не по плану!";
+                return;
+            }
+            int[] ip_int = { 0, 0, 0, 0, 0 };   //для числовых представлений данных
+
+            string tmp = "";                    //вспомогательная строка
+            int j = 0; int i = 0;
+
+            short sig_check_d = 0;              //проверка формата
+            short sig_check_s = 0;
+            for (j = 0; j < data.Length; j++)
+            {
+                if (data[j] == '.')
+                    sig_check_d++;
+                else if (data[j] == '/')
+                    sig_check_s++;
+            }
+            if (sig_check_d != 3 && sig_check_s != '/')
+            {
+                textBox2.Text += "Нет, это так не работает, попробуйте как-то так: 10.10.10.10/25";
+                return;
+            }
+
+            for (j = 0; j < 4; j++)   //заполнение четырёх байтов
+            {
+                while (true)
+                {
+                    if (data[i] == '.' || data[i] == '/')
+                        break;
+                    tmp += data[i];
+                    i++;
+                }
+                i++;
+                try
+                {
+                    ip_int[j] = Int32.Parse(tmp);
+                }
+                catch(Exception)
+                {
+                    textBox2.Text += "Вы ввели некорректные данные, не надо так!";
+                    return;
+                }
+                tmp = "";
+                
+                if(ip_int[j] > 255)   //проверка на величину байта
+                {
+                    textBox2.Text += "Не надо вводить числа больше 255, мы такое осуждаем!";
+                    return;
+                }
+            }
+            while (i < data.Length)   //заполнение маски
+            {
+                tmp += data[i];
+                i++;
+            }
+            ip_int[4] = Int32.Parse(tmp);
+
+            if (ip_int[4] > 32)   //проверка на величину маски
+            {
+                textBox2.Text += "Маска не может быть больше 32, переделывайте!";
+                return;
+            }
+
+            int mask = ip_int[4];
+            int[] m = { 0, 0, 0, 0 };
+            for (i = 0; i < 4; i++)
+            {
+                if (mask >= 8)
+                    m[i] = 255;
+                else
+                {
+                    switch (mask)
+                    {
+                        case 7:
+                            m[i] = 254;
+                            break;
+                        case 6:
+                            m[i] = 252;
+                            break;
+                        case 5:
+                            m[i] = 248;
+                            break;
+                        case 4:
+                            m[i] = 240;
+                            break;
+                        case 3:
+                            m[i] = 224;
+                            break;
+                        case 2:
+                            m[i] = 192;
+                            break;
+                        case 1:
+                            m[i] = 128;
+                            break;
+                    }
+                }
+                mask = mask - 8;
+            }
+
+            
+            textBox2.Text += "Mask: ";
+            //вывод данных
+            for (j = 0; j < 4; j++)
+            {
+                tmp = Convert.ToString(m[j]);
+                textBox2.Text += tmp;
+                if (j != 3)
+                    textBox2.Text += ".";
+                else
+                    textBox2.Text += "";
+            }
+            textBox2.Text += "                                                                                                                                                    ";
+            
+            textBox2.Text += "Network: ";
+            //расчёты Network
+            int[] net = { 0, 0, 0, 0 };
+            for (i = 0; i < 4; i++)
+            {
+                net[i] = ip_int[i] & m[i];
+            }
+            //вывод данных
+            for (j = 0; j < 4; j++)
+            {
+                tmp = Convert.ToString(net[j]);
+                textBox2.Text += tmp;
+                if (j != 3)
+                    textBox2.Text += ".";
+                else
+                    textBox2.Text += "";
+            }
+            textBox2.Text += "                                                                                                                                                    ";
+
+            textBox2.Text += "Broadcast: ";
+            //расчёты Broadcast
+            int[] broadcast = { 0, 0, 0, 0 };
+            for (i = 0; i < 4; i++)
+                broadcast[i] = 256 + (ip_int[i] | ~m[i]);
+            //вывод данных
+            for (j = 0; j < 4; j++)
+            {
+                tmp = Convert.ToString(broadcast[j]);
+                textBox2.Text += tmp;
+                if (j != 3)
+                    textBox2.Text += ".";
+                else
+                    textBox2.Text += "";
+            }
+            textBox2.Text += "                                                                                                                                               ";
+
+            textBox2.Text += "Hosts: ";
+            //расчёты Hosts
+            double hosts_count = Math.Pow(2, (32 - ip_int[4]));
+            //вывод данных
+            tmp = Convert.ToString(hosts_count);
+            textBox2.Text += tmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
